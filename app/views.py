3b1d22679b0247df4e6b22053e3242b7d9f8095a
shivam5799 <115656331+shivam5799@users.noreply.github.com>
2023-03-16 19:13:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def Shree(request):
+    return render(request,'Shree.html')
+
+
+def Akash(request):
+    return render(request,'Akash.html')
